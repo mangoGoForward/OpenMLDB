b0747f7fd3117ba8cd6442a1e27960868300d889
mango <xu.weiKyrie@foxmail.com>
2022-09-23 15:46:50 +0800

    test unit test.
    Signed-off-by: mango <xu.weiKyrie@foxmail.com>

--- hybridse/src/udf/udf_test.cc
+++ hybridse/src/udf/udf_test.cc
@@ -496,7 +496,7 @@ TEST_F(UdfTest, TimestampToString) {
 
 TEST_F(UdfTest, Locate) {
     {
-        int32_t loc = udf::v1::locate(codec::StringRef("bar"), codec::StringRef("foobarbar"));
+        int32_t loc = udf::v1::locate(openmldb::base::StringRef("bar"), openmldb::base::StringRef("foobarbar"));
         ASSERT_EQ(4, loc);
     }
 }
